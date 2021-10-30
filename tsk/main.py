16650db7c5291bec59e6a9a041737260d937a3a5
Christian Cantarino <31663828+cantarino@users.noreply.github.com>
2021-10-30 14:17:40 -0300

    plotting approximate and base functions

--- tsk/main.py
+++ tsk/main.py
@@ -1,22 +1,50 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def createFunction():
-    x = np.linspace(-10, 10, 1000)
+# Tarefa
+# Utilizando o Método do gradiente mostrados nos slides e vídeos do tópico 3,
+# aproximar a saída para f(x) = x^2
 
+def generate_points():
+    return np.linspace(-10, 10, 1000)
+
+def create_base_function():
+    x = generate_points()
     return (x**2, x)
 
-def putFunction(x, y, label):
-    _, ax = plt.subplots()
+def approximate_function():
+    x = generate_points()
+    return ((2*x)**2, x)
 
-    ax.plot(x, y, label=label)
-    ax.legend(loc='upper right')
+def plot_function(x, y, color, label):
+    plt.plot(x, y, label=label, color=color)
 
-def show():
+def plot_base_function():
+    y,x = create_base_function()
+    plot_function(x, y, 'r', 'f(x) = x²')
+
+def plot_approximate_function():
+    y, x= approximate_function()
+    plot_function(x, y, 'b', 'Curva estimada ajustada')    
+
+def configure_plot():
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.legend(loc='upper right')
+    plt.title('Curva da função f(x)=x² e Curva estimada ajustada')
+
+def show_plot():
     plt.show()
 
-y,x = createFunction()
+def show():
+    configure_plot()
+    show_plot()
 
-putFunction(x, y, 'f(x) = x²')
+def main():
+    plot_base_function()
+    plot_approximate_function()
+    show()
 
-show()
+if __name__ == '__main__':
+    main()
+
